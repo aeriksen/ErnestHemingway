777217245567684c7ebc7a76e38fcdbcc90c1dae
aeriksen <43555481+aeriksen@users.noreply.github.com>
2021-11-18 15:11:22 +0100

    Add files via upload

---
+++ Ernest_Hemingway.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("There is nothing noble\x0Ain being superior\n");
+	printf("to your fellow man;\x0Atrue nobility is being superior\n");
+	printf("to your former self.\x0A-Ernest Hemingway\n");
+	return 0;
+}
